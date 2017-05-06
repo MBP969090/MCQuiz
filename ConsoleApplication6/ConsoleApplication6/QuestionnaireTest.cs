56399653a6bd400dd5526557be785b97937c6ac6
Peter Ganzschow <peter.ganzschow@gmx.de>
2017-05-06 15:04:03 +0200

    add questionnaire test

---
+++ ConsoleApplication6/ConsoleApplication6/QuestionnaireTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ConsoleApplication6
+{
+	[TestFixture]
+	class QuestionnaireTest
+	{
+		Questionnaire q = new Questionnaire(1);
+
+		[SetUp]
+		public void SetupQuestionnaire()
+		{
+			q.AddQuestion(1, "Warum nur?", new List<Dictionary<string, bool>>());
+			q.AddQuestion(2, "Warum nicht?", new List<Dictionary<string, bool>>());
+			q.AddQuestion(3, "Ja warum eigentlich nicht?", new List<Dictionary<string, bool>>());
+		}
+
+		[Test]
+		public void GetQuestionTest()
+		{
+			Assert.AreEqual("Warum nur?",  q.GetCurrentQuestion().GetText());
+		}
+
+		[Test]
+		public void NextQuestionTest()
+		{
+			Question nextQuestion = q.GetNextQuestion();
+			Assert.AreEqual("Warum nicht?", nextQuestion.GetText());
+			Question currentQuestion = q.GetCurrentQuestion();
+			Assert.AreEqual("Warum nicht?", currentQuestion.GetText());
+		}
+
+		[Test]
+		public void PreviousQuestionTest()
+		{
+			Assert.AreEqual("Warum nur?", q.GetPreviousQuestion().GetText());
+			q.GetNextQuestion();
+			q.GetNextQuestion();
+			Assert.AreEqual("Warum nicht?", q.GetPreviousQuestion().GetText());
+		}
+	}
+}
